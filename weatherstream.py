a98bc10ce6452582aa14b608a0edb893a4e2be6b
Kevin Coyle <kvn.coyle@gmail.com>
2020-07-18 16:33:52 -0700

    UPDATE weatherstream

--- weatherstream.py
+++ weatherstream.py
@@ -8,18 +8,18 @@ sense.clear()
 
 try:
       while True:
-      	temp = sense.get_temperature()
-      	celcius = round(temp, 1)
-      	fahrenheit = 1.8 * round(temp, 1)  + 32 
-      	humidity = sense.get_humidity()  
+        temp = sense.get_temperature()
+        celcius = round(temp, 1)
+        fahrenheit = 1.8 * round(temp, 1)  + 32 
+        humidity = sense.get_humidity()  
         humidity = round(humidity, 1)  
         pressure = sense.get_pressure()
         pressure = round(pressure, 1)
         time.sleep(1)
         sense.show_message("Temperature C" + str(temp) 
-        	+ "Humidity:" + str(humidity) 
-        	+ "Pressure:" + str(pressure), 
-        	scroll_speed=(0.08), 
-        	back_colour= [0,0,200])
+            + "Humidity:" + str(humidity) 
+            + "Pressure:" + str(pressure), 
+            scroll_speed=(0.08), 
+            back_colour= [0,0,200])
 except KeyboardInterrupt:
       pass
