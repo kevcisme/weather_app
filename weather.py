80e87d597fbc446fc90b1ee0a4a36a66862aa0a5
Kevin Coyle <kvn.coyle@gmail.com>
2020-07-18 15:52:40 -0700

    UPDATE html and python file

--- weather.py
+++ weather.py
@@ -8,3 +8,14 @@ def index():
     sense = SenseHat()
     celcius = round(sense.get_temperature(), 1)
     fahrenheit = round(1.8 * celcius + 32, 1)
+    humidity = round(sense.get_humidity(), 1)
+    pressure = round(sense.get_pressure(), 1)
+    return render_template('weather.html', 
+    	celcius=celcius, 
+    	fahrenheit=fahrenheit, 
+    	humidity=humidity, 
+    	pressure=pressure)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
