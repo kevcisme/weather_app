4f4b52f466c4c94c3d91cd0893634ff6f358e78d
kevcisme <kvn.coyle@gmail.com>
2021-07-27 22:30:19 -1000

    new new

--- weatherstream.py
+++ weatherstream.py
@@ -2,9 +2,13 @@
 from sense_hat import SenseHat
 import time
 import sys
+import csv
+from datetime import datetime
 
 sense = SenseHat()
 sense.clear()
+thetimestamp = datetime.now()
+delay = 60
 
 try:
       while True:
@@ -17,9 +21,28 @@ try:
         pressure = round(pressure, 1)
         time.sleep(1)
         sense.show_message("Temperature:  " + str(fahrenheit - 3) 
-            + "Humidity:  " + str(humidity) 
-            + "Pressure:  " + str(pressure), 
+            + "Humidity:  " + str(humidity), 
             scroll_speed=(0.08), 
             back_colour= [0,0,200])
 except KeyboardInterrupt:
       pass
+
+def get_sense_data():
+    sense_data = []
+    sense_data.append(fahrenheit)
+    sense_data.append(humidity)
+    sense_data.append(pressure)
+    sense_data.append(datetime.now())
+    return sense_data
+
+with open('temp.csv', 'w', newline='') as f:
+    dwriter = csv.writer(f)
+    dwriter.writerow(['temp', 'humidity', 'pressure', 'time'])
+    while True:
+        data = get_sense_data()
+        dt = data[-1] - thetimestamp
+        if dt.seconds > delay:
+            dwriter.writerow(data)
+            thetimestamp = datetime.now()
+
+
